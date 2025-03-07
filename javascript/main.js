c40f1b7708ce9da47a1cf9355ebb170e323acf68
dylanrusere <dylaneight01@gmail.com>
2025-03-07 12:52:26 +0200

    changes-2

--- javascript/main.js
+++ javascript/main.js
@@ -28,3 +28,80 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offMenu.classList.toggle("active");
 });
+
+// Hero Section
+// Hero Section Carousel
+const heroItems = document.querySelectorAll('.hero-item');
+const heroControls = document.querySelectorAll('.hero-controls button');
+let currentIndex = 0;
+
+    function showHeroItem(index) {
+      heroItems.forEach((item, i) => {
+        item.classList.toggle('active_hero', i === index);
+      });
+
+      heroControls.forEach((button, i) => {
+        button.classList.toggle('active_hero', i === index);
+      });
+    }
+
+    function autoRotateHero() {
+      currentIndex = (currentIndex + 1) % heroItems.length;
+      showHeroItem(currentIndex);
+    }
+
+    heroControls.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        currentIndex = index;
+        showHeroItem(currentIndex);
+      });
+    });
+
+    setInterval(autoRotateHero, 5000);
+
+    var index = 0;
+    var slides = document.querySelectorAll(".slides");
+    var dot = document.querySelectorAll(".dot");
+    var carouselContainer = document.querySelector(".image_slider");
+    let intervalId;
+    
+    function changeSlide(newIndex = index) {
+      if (newIndex < 0) {
+        newIndex = slides.length - 1;
+      }
+    
+      if (newIndex > slides.length - 1) {
+        newIndex = 0;
+      }
+    
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dot[i].classList.remove("active_dot");
+      }
+    
+      slides[newIndex].style.display = "block";
+      dot[newIndex].classList.add("active_dot");
+    
+      index = newIndex;
+    }
+    
+    function startCarousel() {
+      intervalId = setInterval(function() {
+        changeSlide(++index);
+      }, 4000);
+    }
+    
+    function pauseCarousel() {
+      clearInterval(intervalId);
+    }
+    
+    dot.forEach((dotElement, dotIndex) => {
+      dotElement.addEventListener("click", () => {
+        changeSlide(dotIndex);
+      });
+    });
+    
+    carouselContainer.addEventListener("mouseenter", pauseCarousel);
+    carouselContainer.addEventListener("mouseleave", startCarousel);
+    
+    startCarousel();
